cde4a47406f292d76a26d310b2fe503aceb51bb8
Abhijit Ganguly <105243912+Abhijit-011@users.noreply.github.com>
2022-10-01 01:02:38 +0530

    Update Friends_pairing_problem.cpp

--- Contributors/Daily/Friends_pairing_problem.cpp
+++ Contributors/Daily/Friends_pairing_problem.cpp
@@ -18,9 +18,7 @@ class Solution
     	for (int i = 3; i <= n; i++)
     	{ 
     		// using modular arithmentic properties.
-    		c = ( b%m + ( ( (i - 1)%m  *  a%m )%m ) %m )%m ; 
-    		a = b; 
-    		b = c; 
+    		
     	} 
     	return c; 
     }
